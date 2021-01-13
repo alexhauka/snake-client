fea2169c4e481b40bda7673afbdfc9b05992a512
alexhauka <alexhauka@gmail.com>
2021-01-13 17:16:52 +0000

    seperated input function to it's own module and exported. Refactored client and play accordingly.

--- client.js
+++ client.js
@@ -17,24 +17,9 @@ const connect = function() {
   return conn;
 };
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key)
-  })
-  return stdin;
-}
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
-setupInput();
+
 // "Move: up" - move up one square (unless facing down)
 // "Move: down" - move down one square (unless facing up)
 // "Move: left" - move left one square (unless facing right)
