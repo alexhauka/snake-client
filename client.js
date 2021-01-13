989bbb979f471c3c77abb2bba3337361193cfe07
alexhauka <alexhauka@gmail.com>
2021-01-13 17:10:00 +0000

    added userinput function and ctrl+c use

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ const connect = function() {
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: AMH')
+    conn.write('Name: AMH');
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
@@ -16,5 +16,28 @@ const connect = function() {
 
   return conn;
 };
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  })
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
 
 module.exports = connect;
