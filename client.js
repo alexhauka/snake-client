ff43deccf024030e8a0de33d5617cdbc9c402492
alexhauka <alexhauka@gmail.com>
2021-01-13 16:50:47 +0000

    added name message to client

--- client.js
+++ client.js
@@ -3,9 +3,13 @@ const net = require('net');
 const connect = function() {
   const conn = net.createConnection({
     host: '10.0.2.15',
-    port: 50541
+    port: 50541,
   });
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: AMH')
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
