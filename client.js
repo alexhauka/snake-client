c813523031da7a379c7e7eb6e86b90a7493a620d
alexhauka <alexhauka@gmail.com>
2021-01-13 21:09:28 +0000

    successfully connected to multiplayer with controls and Say: message

--- client.js
+++ client.js
@@ -2,8 +2,8 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541,
+    host: '135.23.222.131',
+    port: 50542,
   });
   conn.setEncoding('utf8');
   conn.on('connect', () => {
